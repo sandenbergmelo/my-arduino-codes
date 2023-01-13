488d369d9ea683b12a071cddfa20b1849177c774
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2023-01-13 18:20:01 -0300

    Add some codes

---
+++ songs/giorno-theme/giorno-theme.ino
@@ -0,0 +1,254 @@
+const int speakPin = 4;
+const int buttonPin = 3;
+const int Qnote = 441.1;
+const int Hnote = Qnote*2;
+const int Wnote = Qnote*4;
+const int Enote = Qnote/2;
+const int Snote = Qnote/4;
+const int b4b = 466;
+const int b4 = 494;
+const int c5 = 523;
+const int c5s = 554;
+const int d5 = 587;
+const int d5s = 622;
+const int e5 = 659;
+const int f5 = 699;
+const int f5s = 740;
+const int g5 = 784;
+const int g5s = 831;
+const int a5 = 880;
+const int a5s = 932;
+const int b5 = 988;
+const int c6 = 1047;
+const int c6s = 1109;
+const int d6 = 1175;
+const int d6s = 1245;
+const int e6 = 1319;
+const int f6 = 1397;
+const int f6s = 1480;
+const int g6 = 1568;
+const int g6s = 1661;
+const int a6 = 1760;
+const int a6s = 1865;
+const int b6 = 1976;
+const int c7 = 1093;
+const int c7s = 2217;
+const int d7 = 2349;
+
+void setup(){
+  pinMode(speakPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop(){
+  int pressed = !digitalRead(buttonPin);
+
+  if (pressed) {
+    tone(speakPin, f5s);
+    delay(Qnote+Enote);
+    noTone(speakPin);
+    delay(50);
+  
+    tone(speakPin, d5);
+    delay(Hnote);
+    noTone(speakPin);
+    delay(50);
+  
+    tone(speakPin, d5);
+    delay(Snote);
+    noTone(speakPin);
+    delay(12);
+  
+    tone(speakPin, e5);
+    delay(Snote);
+    noTone(speakPin);
+    delay(50);
+  
+    tone(speakPin, f5);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+  
+    tone(speakPin, e5);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+  
+    tone(speakPin, d5);
+    delay(Enote);
+    noTone(speakPin);
+    delay(50);
+  
+    tone(speakPin, c5s);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+  
+    tone(speakPin, d5);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+  
+    tone(speakPin, e5);
+    delay(Enote);
+    noTone(speakPin);
+    delay(25);
+  
+    tone(speakPin, f5s);
+    delay(Qnote+Enote);
+    noTone(speakPin);
+    delay(50);
+  
+    tone(speakPin, b5);
+    delay(Qnote+Enote);
+    noTone(speakPin);
+    delay(50);
+  
+    tone(speakPin, b4);
+    delay(Enote);
+    noTone(speakPin);
+    delay(25);
+  
+    tone(speakPin, c5s);
+    delay(Enote);
+    noTone(speakPin);
+    delay(50);
+  
+    tone(speakPin, d5);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+  
+    tone(speakPin, e5);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+  
+    tone(speakPin, d5);
+    delay(Enote);
+    noTone(speakPin);
+    delay(25);
+  
+    tone(speakPin, c5s);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+  
+    tone(speakPin, a5);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+  
+    tone(speakPin, g5);
+    delay(Enote);
+    noTone(speakPin);
+    delay(50);
+
+    tone(speakPin, f6s);
+    delay(Qnote+Enote);
+    noTone(speakPin);
+    delay(50);
+
+    tone(speakPin, d6);
+    delay(Hnote);
+    noTone(speakPin);
+    delay(50);
+
+    tone(speakPin, d6);
+    delay(Snote);
+    noTone(speakPin);
+    delay(12.5);
+
+    tone(speakPin, e6);
+    delay(Snote);
+    noTone(speakPin);
+    delay(50);
+
+    tone(speakPin, f6);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+
+    tone(speakPin, e6);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+
+    tone(speakPin, d6);
+    delay(Enote);
+    noTone(speakPin);
+    delay(50);
+
+    tone(speakPin, c6s);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+
+    tone(speakPin, d6);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+
+    tone(speakPin, e6);
+    delay(Enote);
+    noTone(speakPin);
+    delay(25);
+
+    tone(speakPin, f6s);
+    delay(Qnote+Enote);
+    noTone(speakPin);
+    delay(50);
+
+    tone(speakPin, b6);
+    delay(Qnote+Enote);
+    noTone(speakPin);
+    delay(50);
+
+    tone(speakPin, b6);
+    delay(Enote);
+    noTone(speakPin);
+    delay(50);
+
+    tone(speakPin, c7s);
+    delay(Enote);
+    noTone(speakPin);
+    delay(25);
+
+    tone(speakPin, d7);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+
+    tone(speakPin, g6);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+
+    tone(speakPin, f6s);
+    delay(Enote);
+    noTone(speakPin);
+    delay(50);
+
+    tone(speakPin, f6);
+    delay(Snote+Enote);
+    noTone(speakPin);
+    delay(25);
+
+    tone(speakPin, d7);
+    delay(Enote+Snote);
+    noTone(speakPin);
+    delay(25);
+
+    tone(speakPin, a6s);
+    delay(Enote);
+    noTone(speakPin);
+    delay(25);
+
+    tone(speakPin, b6);
+    delay(Qnote);
+    noTone(speakPin);
+    delay(300);
+  }
+}
+
+  
---
+++ songs/lost_woods/lost_woods.ino
@@ -0,0 +1,60 @@
+//Initialize buzzer, button
+const int buzzerPin = 4;
+const int buttonPin = 3;
+
+// Length must equal the total number of notes and spaces 
+const int songLength = 88;
+
+// I directly input the frequencies that need to be played
+// Alternatively, you could set each frequency to a particular note
+// 0 represent rests
+int notes[] = {698,880,988,698,880,988,698,880,988,1319,1175,988,1047,988,784,659,587,659,784,659,
+698,880,988,698,880,988,698,880,988,1319,1175,988,1047,1319,988,784,988,784,587,659,
+587,659,698,784,880,988,1047,988,659,698,784,880,988,1047,1175,1319,1397,1568,
+587,659,698,784,880,988,1047,988,659,
+784,698,880,784,988,880,1047,988,1175,1047,1319,1175,1397,1319,1319,1397,1175,1319,
+0,0,0
+};
+
+// Beats is an array values for each note and rest.
+// A "1" represents a quarter-note, 2 a half-note, etc.
+// Rests need a length as well.
+int beats[] = {2,2,4,2,2,4,2,2,2,2,4,2,2,2,2,8,2,2,2,10,
+2,2,4,2,2,4,2,2,2,2,4,2,2,2,2,8,2,2,2,10,
+2,2,4,2,2,4,2,2,8,2,2,4,2,2,4,2,2,8,
+2,2,4,2,2,4,2,2,8,
+2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,2,6,
+4,2,2
+};
+
+// To make the song play faster, decrease this value.
+int tempo = 120;
+
+void setup()  {
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() 
+{
+  //Loop through the array
+  int i, duration;
+  int pressed = !digitalRead(buttonPin);
+  
+  if (pressed) {
+    for (i = 0; i < songLength; i++) {
+      duration = beats[i] * tempo;  // length of note/rest in ms
+      //If rest, then pause for specified time
+      if (notes[i] == 0) {
+        delay(duration);
+      }
+      //Else, play note for specified time
+      else {
+        tone(buzzerPin, notes[i], duration);
+        delay(duration);
+      }
+      //Brief pause between notes
+      delay(tempo/10);
+    }
+  }
+}
