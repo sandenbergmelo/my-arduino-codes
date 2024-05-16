78e2e2bd5743dec97d8ab50431b041bd3756f4fa
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2024-05-16 16:34:06 -0300

    Update digispark scripts

--- digispark/rickroll/rickroll.ino
+++ digispark/rickroll/rickroll.ino
@@ -4,16 +4,16 @@ void setup() {
   //empty
 }
 void loop() {
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_F2, MOD_ALT_LEFT);
   DigiKeyboard.delay(600);
-  DigiKeyboard.print("https://youtu.be/dQw4w9WgXcQ");
+  DigiKeyboard.print("firefox https://youtu.be/dQw4w9WgXcQ");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(5000);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_F2, MOD_ALT_LEFT);
   DigiKeyboard.delay(1000);
-  DigiKeyboard.print("http://fakeupdate.net/win10ue");
+  DigiKeyboard.print("firefox http://fakeupdate.net/win10ue");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_F11);
